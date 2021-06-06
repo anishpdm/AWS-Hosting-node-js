fece6d443cd235be3ffff1d5d65d8ee9b19185e1
anishpdm <anish.vilayil.s@gmail.com>
2021-06-06 10:53:07 +0530

    chnages

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to my Website')
 })
+
+
 
 app.listen(port, () => {
   console.log('Running on PORT 3000')
